2a652c42e9724b4dbc16814d13b69d0df3e241f6
Micah <micah.head@csedge.org>
2025-07-08 12:03:11 -0700

    finished my first lit madlib

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("This is my first Repo!")
+# print("This is my first Repo!")
+# Don't add spaces when inputing answers!!!!!
+
+adj = input("Give me an adjective earthling!" )
+print("Oh.." + adj + " " + "..that's not a bad one.")
+plurN = input("Give me a plural noun")
+relative = input("Go ahead and think of a relative and type it in")
+noun1 = input("Think of a noun..and TYPE IT IN")
+adj1 = input("Give me an adjective!! Please, we're almost done")
+noun2 = input("Okay last request. Give me a noun")
+finalM = input("Okay this madlib is lit asf brah. Want to see it?")
+print("LITTTT okay here it is brochacho:", "A", adj, "spaceship embarked on a wild mission to uncover the secret of the",plurN, 
+"Aboard the spaceship, Lieutenant",relative,"and his crew faced unexpected challenges as they navigated through star systems filled with",
+noun1, "While exploring a distant planet, they encounter a particularly", adj1,
+ "and I mean", adj, "alien species who held the key to solving the enigmatic puzzle of the universe.",
+ "With determination and teamwork, they unraveled the galactic riddle,forver changing their understanding of the",noun2,"around them.")
